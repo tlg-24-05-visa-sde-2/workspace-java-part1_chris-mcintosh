ede8aa8ff8a0e93514ee57ebfb66c8aed92c11a0
Christopher_McIntosh <ChristopherM@tlgcohort.com>
2024-07-12 10:51:31 -0500

    Lab 10.1 throw unchecked exception

--- Lab10.1/src/com/hr/personnel/HourlyEmployee.java
+++ Lab10.1/src/com/hr/personnel/HourlyEmployee.java
@@ -14,6 +14,7 @@ public class HourlyEmployee extends Employee {
     // fields
     private double rate;
     private double hours;
+    public static final double FEDERAL_MINIMUM_WAGE = 15.0;
 
     // constructors
     public HourlyEmployee() {
@@ -23,7 +24,8 @@ public class HourlyEmployee extends Employee {
         super(name, hireDate);  // delegate to superclass ctor for name, hireDate
     }
 
-    public HourlyEmployee(String name, LocalDate hireDate, double rate, double hours) {
+    public HourlyEmployee(String name, LocalDate hireDate, double rate, double hours)
+    throws IllegalArgumentException {
         this(name, hireDate);   // delegate to neighboring ctor for name, hireDate
         setRate(rate);          // handle rate here, by delegating to setter
         setHours(hours);        // handle hours here, by delegating to setter
@@ -47,7 +49,13 @@ public class HourlyEmployee extends Employee {
     }
 
     public void setRate(double rate) {
-        this.rate = rate;
+        if (rate >= FEDERAL_MINIMUM_WAGE) {
+            this.rate = rate;
+        }
+        else {
+            throw new IllegalArgumentException ("Illegal wage:" + rate + " Federal minimum wage is 15.0");
+        }
+
     }
 
     public double getHours() {
