b21a1b5f9babb89443f1429140f241a40893be72
Christopher_McIntosh <ChristopherM@tlgcohort.com>
2024-07-15 12:19:59 -0500

    More Homework

--- Shape/src/edu/geometry/client/ShapeClient.java
+++ Shape/src/edu/geometry/client/ShapeClient.java
@@ -2,18 +2,32 @@ package edu.geometry.client;
 
 import edu.geometry.Circle;
 import edu.geometry.Rectangle;
+import edu.geometry.Shape;
 import edu.geometry.Triangle;
+
+import static java.lang.Math.round;
 
 public class ShapeClient {
     public static void main(String[] args) {
         // Create instances of different shapes
-        Circle circle = new Circle(5.0);
-        Rectangle rectangle = new Rectangle(4.0, 6.0);
-        Triangle triangle = new Triangle(3.0, 7.0);
+        Shape circle = new Circle(3.0);
+        Shape rectangle = new Rectangle(4.0, 6.0);
+        Shape triangle = new Triangle(3.0, 7.0);
 
         // Display the areas of the shapes
         System.out.println("Circle area: " + circle.calculateArea());
         System.out.println("Rectangle area: " + rectangle.calculateArea());
         System.out.println("Triangle area: " + triangle.calculateArea());
+        System.out.println();
+
+        Shape[] shapes = new Shape[3];
+        shapes[0] = circle;
+        shapes[1] = rectangle;
+        shapes[2] = triangle;
+
+
+        for (Shape s : shapes) {
+            System.out.println(String.format("%.2f", s.calculateArea()));
+        }
     }
 }
