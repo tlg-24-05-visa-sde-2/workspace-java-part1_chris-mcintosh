281b233d4d29d1821951587ae4fb665da568d22c
Christopher_McIntosh <ChristopherM@tlgcohort.com>
2024-07-16 14:59:21 -0500

    Read and Write textfiles using Decorator (Buffered Reader, PrintWriter)

--- Lab12.1/src/com/poetry/io/PoemClient.java
+++ Lab12.1/src/com/poetry/io/PoemClient.java
@@ -8,8 +8,7 @@
 
 package com.poetry.io;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
 
 public class PoemClient {
 
@@ -18,7 +17,7 @@ public class PoemClient {
      */
     public static void main(String[] args) {
         // readPoem();
-        // writePoem();
+         writePoem();
     }
 
     /**
@@ -34,9 +33,10 @@ public class PoemClient {
      * The try-with-resources below allows you to initialize the stream and auto-close it.
      */
     private static void readPoem() {
-        // TODO: initialize 'reader' variable and complete the try block
-        try (BufferedReader reader = null) {
-
+        try (BufferedReader reader = new BufferedReader(new FileReader("famous-poem.txt"))) {
+            String line; while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -55,6 +55,14 @@ public class PoemClient {
      * Use a try-with-resources to initialize the stream and auto-close it.
      */
     private static void writePoem() {
-        // TODO
+        try (PrintWriter out = new PrintWriter(new FileWriter("haiku.txt"))) {
+            out.println("Forgotten brackets,");
+            out.println("Syntax errors haunt the code—");
+            out.println("Fix one, five more grow. :(");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
